39f10295c01f3f2986530f56d312c06fe2aa8aa2
Fred Wu <ifredwu@gmail.com>
2016-08-27 18:10:16 +1000

    Implemented `Hand.FourOfAKind.valid?/1`

--- lib/poker_hands/hand/four_of_a_kind.ex
+++ lib/poker_hands/hand/four_of_a_kind.ex
@@ -1,2 +1,21 @@
 defmodule PokerHands.Hand.FourOfAKind do
+  @doc """
+  ## Examples
+
+      iex> PokerHands.Hand.FourOfAKind.valid?(
+      iex>   PokerHands.DealtHand.init("2D 2H 2S 7D 2C")
+      iex> )
+      true
+
+      iex> PokerHands.Hand.FourOfAKind.valid?(
+      iex>   PokerHands.DealtHand.init("2D 2H 3S 7D 2C")
+      iex> )
+      false
+  """
+  def valid?(dealt_hand) do
+    dealt_hand.grouped_card_values
+    |> Kernel.hd()
+    |> Kernel.length()
+    |> Kernel.==(4)
+  end
 end
