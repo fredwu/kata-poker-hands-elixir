998f17281e2f60400d4d50106715e14189d64478
Fred Wu <ifredwu@gmail.com>
2016-08-27 18:18:33 +1000

    Implemented `Hand.FullHouse.valid?/1`

--- lib/poker_hands/hand/full_house.ex
+++ lib/poker_hands/hand/full_house.ex
@@ -1,2 +1,21 @@
 defmodule PokerHands.Hand.FullHouse do
+  @doc """
+  ## Examples
+
+      iex> PokerHands.Hand.FullHouse.valid?(
+      iex>   PokerHands.DealtHand.init("2D 2H 7S 7D 2C")
+      iex> )
+      true
+
+      iex> PokerHands.Hand.FullHouse.valid?(
+      iex>   PokerHands.DealtHand.init("2D 2H 3S 7D 2C")
+      iex> )
+      false
+  """
+  def valid?(dealt_hand) do
+    [three_of_a_kind | remaining] = dealt_hand.grouped_card_values
+    [pair | _remaining]           = remaining
+
+    Kernel.length(three_of_a_kind) == 3 && Kernel.length(pair) == 2
+  end
 end
