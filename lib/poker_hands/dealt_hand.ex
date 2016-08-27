e5a3b42847aa05317337086bbd9f52153ba9b9d7
Fred Wu <ifredwu@gmail.com>
2016-08-27 16:37:37 +1000

    Turned `DealtHand` into a struct

--- lib/poker_hands/dealt_hand.ex
+++ lib/poker_hands/dealt_hand.ex
@@ -1,17 +1,29 @@
 defmodule PokerHands.DealtHand do
+  defstruct type: nil,
+            value: 0,
+            cards: []
+
   alias PokerHands.Card
 
   @doc """
   ## Examples
 
       iex> PokerHands.DealtHand.init("2H 4S AH")
-      [
-        %PokerHands.Card{value: "2", suit: "H", int_value: 2},
-        %PokerHands.Card{value: "4", suit: "S", int_value: 4},
-        %PokerHands.Card{value: "A", suit: "H", int_value: 14}
-      ]
+      %PokerHands.DealtHand{
+        cards: [
+          %PokerHands.Card{value: "2", suit: "H", int_value: 2},
+          %PokerHands.Card{value: "4", suit: "S", int_value: 4},
+          %PokerHands.Card{value: "A", suit: "H", int_value: 14}
+        ]
+      }
   """
   def init(denotation_string) do
+    %__MODULE__{
+      cards: init_cards(denotation_string)
+    }
+  end
+
+  defp init_cards(denotation_string) do
     denotation_string
     |> String.split(" ")
     |> Enum.map(fn(denotation) -> Card.init(denotation) end)
