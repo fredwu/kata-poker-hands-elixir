531f3a312f9656d8f8398ff7a1dfea60617a18d7
Fred Wu <ifredwu@gmail.com>
2016-08-27 15:56:27 +1000

    `PokerHands.init/1` to `PokerHands.DealtHand.init/1`

--- lib/poker_hands.ex
+++ lib/poker_hands.ex
@@ -1,19 +1,2 @@
 defmodule PokerHands do
-  alias PokerHands.Card
-
-  @doc """
-  ## Examples
-
-      iex> PokerHands.init("2H 4S AH")
-      [
-        %PokerHands.Card{value: "2", suit: "H", int_value: 2},
-        %PokerHands.Card{value: "4", suit: "S", int_value: 4},
-        %PokerHands.Card{value: "A", suit: "H", int_value: 14}
-      ]
-  """
-  def init(denotation_string) do
-    denotation_string
-    |> String.split(" ")
-    |> Enum.map(fn(denotation) -> Card.init(denotation) end)
-  end
 end
---
+++ test/lib/poker_hands/dealt_hand_test.exs
@@ -0,0 +1,5 @@
+defmodule PokerHands.DealtHandTest do
+  use ExUnit.Case
+
+  doctest PokerHands.DealtHand
+end
