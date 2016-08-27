2da6cbb6083679641980518e5311280ef0f4e3bb
Fred Wu <ifredwu@gmail.com>
2016-08-27 16:52:08 +1000

    Implemented `Hand.Flush.valid?/1`

--- lib/poker_hands/hand/flush.ex
+++ lib/poker_hands/hand/flush.ex
@@ -1,2 +1,34 @@
 defmodule PokerHands.Hand.Flush do
+  @doc """
+  ## Examples
+
+      iex> PokerHands.Hand.Flush.valid?(
+      iex>   %PokerHands.DealtHand{
+      iex>     cards: [
+      iex>       %PokerHands.Card{suit: "H"},
+      iex>       %PokerHands.Card{suit: "H"},
+      iex>       %PokerHands.Card{suit: "H"}
+      iex>     ]
+      iex>   }
+      iex> )
+      true
+
+      iex> PokerHands.Hand.Flush.valid?(
+      iex>   %PokerHands.DealtHand{
+      iex>     cards: [
+      iex>       %PokerHands.Card{suit: "H"},
+      iex>       %PokerHands.Card{suit: "C"},
+      iex>       %PokerHands.Card{suit: "H"}
+      iex>     ]
+      iex>   }
+      iex> )
+      false
+  """
+  def valid?(dealt_hand) do
+    dealt_hand.cards
+    |> Enum.map(&(&1.suit))
+    |> Enum.uniq()
+    |> Kernel.length()
+    |> Kernel.==(1)
+  end
 end
