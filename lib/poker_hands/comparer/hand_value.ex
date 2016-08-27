f1826d0e72a2b195dd353a287eb12be3882ea7a0
Fred Wu <ifredwu@gmail.com>
2016-08-27 17:07:46 +1000

    Reimplemented `Comparer.HandValue` to compare list of numbers

--- lib/poker_hands/comparer/hand_value.ex
+++ lib/poker_hands/comparer/hand_value.ex
@@ -2,16 +2,23 @@ defmodule PokerHands.Comparer.HandValue do
   @doc """
   ## Examples
 
-      iex> PokerHands.Comparer.HandValue.compare(100, 80)
-      {:left, 100}
+      iex> PokerHands.Comparer.HandValue.compare([10, 2], [8, 7])
+      :left
 
-      iex> PokerHands.Comparer.HandValue.compare(100, 120)
-      {:right, 120}
+      iex> PokerHands.Comparer.HandValue.compare([10, 2], [10, 7])
+      :right
 
-      iex> PokerHands.Comparer.HandValue.compare(100, 100)
-      {:tie, 100}
+      iex> PokerHands.Comparer.HandValue.compare([10, 2], [10, 2])
+      :tie
   """
-  def compare(left, right) when left == right, do: {:tie, left}
-  def compare(left, right) when left > right,  do: {:left, left}
-  def compare(left, right) when left < right,  do: {:right, right}
+  def compare(left, right) do
+    left
+    |> Enum.zip(right)
+    |> Enum.find(fn({l, r}) -> l != r end)
+    |> left_or_right()
+  end
+
+  defp left_or_right(nil),                             do: :tie
+  defp left_or_right({left, right}) when left > right, do: :left
+  defp left_or_right({left, right}) when left < right, do: :right
 end
