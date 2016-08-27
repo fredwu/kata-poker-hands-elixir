c518e24390b9f816d5e374f92dc405d9194a8d69
Fred Wu <ifredwu@gmail.com>
2016-08-27 15:11:28 +1000

    `Card` now tracks an int value, useful for ranking

--- lib/poker_hands.ex
+++ lib/poker_hands.ex
@@ -6,9 +6,9 @@ defmodule PokerHands do
 
       iex> PokerHands.init("2H 4S AH")
       [
-        %PokerHands.Card{value: "2", suit: "H"},
-        %PokerHands.Card{value: "4", suit: "S"},
-        %PokerHands.Card{value: "A", suit: "H"}
+        %PokerHands.Card{value: "2", suit: "H", int_value: 2},
+        %PokerHands.Card{value: "4", suit: "S", int_value: 4},
+        %PokerHands.Card{value: "A", suit: "H", int_value: 14}
       ]
   """
   def init(denotation_string) do
--- lib/poker_hands/card.ex
+++ lib/poker_hands/card.ex
@@ -1,18 +1,35 @@
 defmodule PokerHands.Card do
-  defstruct value: "", suit: ""
+  defstruct value: "", suit: "", int_value: 0
+
+  @value_mapping %{
+    "T" => 10,
+    "J" => 11,
+    "Q" => 12,
+    "K" => 13,
+    "A" => 14
+  }
+
+  @value_mapping_keys Map.keys(@value_mapping)
 
   @doc """
   ## Examples
 
       iex> PokerHands.Card.init("AH")
-      %PokerHands.Card{value: "A", suit: "H"}
+      %PokerHands.Card{value: "A", suit: "H", int_value: 14}
 
       iex> PokerHands.Card.init("6S")
-      %PokerHands.Card{value: "6", suit: "S"}
+      %PokerHands.Card{value: "6", suit: "S", int_value: 6}
   """
   def init(denotation) do
     [value, suit] = String.split(denotation, "", trim: true)
 
-    %__MODULE__{value: value, suit: suit}
+    %__MODULE__{
+      value:     value,
+      suit:      suit,
+      int_value: int_value(value)
+    }
   end
+
+  defp int_value(x) when x in @value_mapping_keys, do: @value_mapping[x]
+  defp int_value(x),                               do: String.to_integer(x)
 end
